fcfeac842d69ba0f3556fbbe5d91cbe9395ee027
NotADonut666 <73786233+NotADonut666@users.noreply.github.com>
2021-08-23 11:44:31 +0300

    Create passGen.js

---
+++ PasswordGenerator/passGen.js
@@ -0,0 +1,40 @@
+import { writeFileSync, readFileSync } from 'fs';
+
+const symbols = JSON.parse(readFileSync('./symbols.json')).symbols;
+const argsName = {
+  foldername: 2, 
+  passwordLength: 3
+}
+
+const foldername = process.argv[argsName.foldername];
+const passwordLength = process.argv[argsName.passwordLength]; 
+
+const maxNumValue = 9;
+
+function randomPassword(PassLength, Alphabet)
+{
+  const _BiggestAlphabetIndex = Alphabet.length - 1; 
+
+  let pass = "";
+
+  for (let i = 0; i < PassLength; i++)
+   {
+    let passNum = Math.round(Math.random() * _BiggestAlphabetIndex);
+    
+    if(Math.round(Math.random()) === 0)
+      {
+        pass += passNum*passNum % maxNumValue;
+      }
+    else
+      {
+        pass += Alphabet[passNum];
+      }
+  }
+
+  console.log(pass);
+
+  return pass;
+}
+
+let password = randomPassword(passwordLength, symbols);
+writeFileSync(`../${foldername}/password`,password, "utf-8"); 
