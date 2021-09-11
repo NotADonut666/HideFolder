f8ee0b0f731287bd7fcc53d735d337829c81784b
NotADonut666 <73786233+NotADonut666@users.noreply.github.com>
2021-09-11 17:41:20 +0300

    Add error processing

--- main.bat
+++ main.bat
@@ -2,7 +2,13 @@
 cls
 title Hide Folder
 
-set  standartPasswordLength="16"
+if NOT EXIST .\PasswordGenerator (
+  set CurrentError="main.bat must be in original folder"
+  goto FAIL
+)
+
+set standartPasswordLength="24"
+set CurrentError="NONE"
 
 echo Enter the folder name
 set /p "foldername=>"
@@ -14,7 +20,6 @@ if NOT EXIST %foldername% goto MDLOCKER
 :CONFIRM
 echo Are you sure you want to lock the folder(Y/N)
 set /p "cho=>"
-
 if %cho%==Y goto LOCK
 if %cho%==y goto LOCK
 if %cho%==n goto END
@@ -27,9 +32,7 @@ goto CONFIRM
 echo Enter the password for the folder (You can get random password)
 set /p "pass=>"
 
-
 ren %foldername% %FullFolderName%
-
 
 attrib +h +s %FullFolderName%
 if %pass%==random goto RANDOM_PASSWORD
@@ -47,9 +50,11 @@ attrib -h -s  %FullFolderName%\password
 echo Enter password to unlock folder:
  <%FullFolderName%\password set /p "pass="
 
-set/p "EnteredPass=>"
-if NOT %EnteredPass%== %pass% echo %pass%
-if NOT %EnteredPass%== %pass% goto FAIL
+set /p "EnteredPass=>"
+if NOT %EnteredPass%== %pass% (
+  set CurrentError="Invalid Password"
+  goto FAIL
+)
 
 attrib -h -s %FullFolderName%
 ren %FullFolderName% %foldername%
@@ -57,10 +62,12 @@ ren %FullFolderName% %foldername%
 del "%foldername%\password"
 echo Folder Unlocked successfully
 pause
-goto FAIL
+goto End
 
 :FAIL
-echo Invalid password
+color 4
+echo ERROR: %CurrentError%
+pause
 goto End
 
 :RANDOM_PASSWORD
@@ -69,8 +76,6 @@ node passGen %FullFolderName%  %standartPasswordLength%
 cd ..\
 pause
 attrib +h +s  %FullFolderName%\password
-
-
 goto End
 
 :MDLOCKER
